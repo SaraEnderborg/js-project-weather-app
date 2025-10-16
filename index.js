d794a8bd4b824cdca077d3e3e6aa76bd7d7e8c8c
Sara Enderborg <sara_enderborg@outlook.com>
2025-10-16 15:57:19 +0200

    This commit will add API

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+const URL = "https://opendata-download-metfcst.smhi.se/api/category/snow1g/version/1/geotype/point/lon/18.062639/lat/59.329468/data.json?timeseries=24";
+let currentWeather;
+const fetchWeather = async () => {
+    try {
+        const response = await fetch(URL);
+        if (!response.ok)
+            throw new Error(`HTTP error: ${response.status}`);
+        const data = await response.json();
+        currentWeather = {
+            airTemp: data.timeSeries[0].data.air_temperature,
+            condition: data.timeSeries[0].data.symbol_code
+        };
+        console.log(currentWeather);
+        currentWeather();
+    }
+    catch (error) {
+        console.log(`Error caught, ${error}`);
+        fetchWeather();
+        // const degreesContainer = document.querySelector(".degrees")
+        const degreesContainer = document.querySelector(".degrees");
+        const displayDegrees = (airTemp) => {
+            degreesContainer.innerHTML = `
+    <h1>${airTemp}</h1>
+    <h2>°C</h2>
+  `;
+        };
+        displayDegrees(currentWeather.airTemp);
+    }
+    console.log(displayDegrees);
+};
+export {};
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,4 +1,43 @@
+const URL = "https://opendata-download-metfcst.smhi.se/api/category/snow1g/version/1/geotype/point/lon/18.062639/lat/59.329468/data.json?timeseries=24"
+
 interface currentWeatherData {
-  airtemp: number,
+  airTemp: number,
   condition: string
+}
+
+let currentWeather: currentWeatherData
+
+const fetchWeather = async () => {
+  try {
+    const response = await fetch(URL)
+    if (!response.ok) throw new Error(`HTTP error: ${response.status}`) 
+
+      const data = await response.json() 
+
+
+      currentWeather = {
+        airTemp: data.timeSeries[0].data.air_temperature,
+        condition: data.timeSeries[0].data.symbol_code
+      } 
+      
+    console.log(currentWeather)
+
+
+  } catch (error) {
+    console.log(`Error caught, ${error}`)
+    
+    fetchWeather()
+
+// const degreesContainer = document.querySelector(".degrees")
+const degreesContainer: any = document.querySelector(".degrees")
+
+const displayDegrees: any = (airTemp: number) => {
+  degreesContainer.innerHTML = `
+    <h1>${airTemp}</h1>
+    <h2>°C</h2>
+  `
+}
+displayDegrees(currentWeather.airTemp)
+}
+console.log(displayDegrees)
 }
